f901c4e8c1148eba05d4d54403262fc511a99691
rleongPd5 <rleong00@stuy.edu>
2018-09-16 00:56:45 -0400

    Fixed print command

--- Hello.java
+++ Hello.java
@@ -1,4 +1,7 @@
 //Prints out "Hello my name is Rachel Leong, plus a greeting."
 
-System.out.println("Hello my name is Rachel Leong, and I say good evening from Brooklyn, NY while drinking a cup of water.");
-
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello my name is Rachel Leong, good evening from Brooklyn.");
+    }
+}
