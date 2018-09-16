7f6dfb2bf94e6abbf6c378aa3bc423cb2d5d53b8
rleongPd5 <rleong00@stuy.edu>
2018-09-15 23:55:30 -0400

    Added print name and greeting

---
+++ Hello.java
@@ -0,0 +1,4 @@
+//Prints out "Hello my name is Rachel Leong, plus a greeting."
+
+System.out.println("Hello my name is Rachel Leong, and I say good evening from Brooklyn, NY while drinking a cup of water.");
+
